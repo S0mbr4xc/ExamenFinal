7270f9c2378df956017a9d121a5789c44e21bd4f
S0mbr4xc <s3._xc@hotmail.com>
2024-02-06 10:05:34 -0500

    Configuracion

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { VistaComponent } from './pages/vista/vista.component';
+import { IngresoComponent } from './pages/ingreso/ingreso.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "paginas/ingreso", component: IngresoComponent},
+  {path: "paginas/vista", component: VistaComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { IngresoComponent } from './pages/ingreso/ingreso.component';
+import { VistaComponent } from './pages/vista/vista.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    IngresoComponent,
+    VistaComponent
   ],
   imports: [
     BrowserModule,
